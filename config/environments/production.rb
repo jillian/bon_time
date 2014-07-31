310f1c9306ce9c90747d5546f70a6121a0060e18
jmac84242 <jillian.macnaughton@gmail.com>
2014-07-31 15:39:05 +0100

    changing TimeOn constants

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-TimeOn::Application.configure do
+BonTime::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-TimeOn::Application.configure do
+BonTime::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
